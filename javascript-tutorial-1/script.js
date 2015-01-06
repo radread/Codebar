b924c130d957d1f21a4f786d080f5cf56b038176
radread <ellen.buddle@gmail.com>
2015-01-06 23:52:05 +0000

    Created first few variables and expressions

--- javascript-tutorial-1/script.js
+++ javascript-tutorial-1/script.js
@@ -1,1 +1,20 @@
+//Check the console is working
 console.log("Hello! This is my first line of Javascript!");
+
+//Set a variable and write an expression
+var name = "Shehryar";
+
+console.log(name + " is amazing!"); // this is an expression
+
+//Set a numerical variable and write another expression
+var pi = 3.14;
+
+console.log("The value of pi: " + pi);
+
+//Set a variable then use addition and the first variable to create another.
+//Write an expression about it
+var year = 2014
+var nextYear = year + 1;
+
+console.log("We are in " + year + ", but " + nextYear + " is just around the corner!");
+
