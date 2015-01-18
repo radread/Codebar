4eeb587e7717a9eb4d823c50e457f336acd90de8
radread <ellen.buddle@gmail.com>
2015-01-18 18:41:27 +0000

    deleted undefined variables content2, content3

--- javascript-tutorial-2/london-script.js
+++ javascript-tutorial-2/london-script.js
@@ -104,9 +104,7 @@ function displayUniversities() {
 
 function displayAverageRent() {
 	var rent = document.createElement("p");
-	var content1 = document.createTextNode("Average monthly rent: £" + london.averageRent);
-	rent.appendChild(content1);
-	rent.appendChild(content2);
-	rent.appendChild(content3);
+	var content = document.createTextNode("Average monthly rent: £" + london.averageRent);
+	rent.appendChild(content);
 	document.body.appendChild(rent);
 }
