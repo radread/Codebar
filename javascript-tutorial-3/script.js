7f60bb2adaca56e88d308fd08497537166730898
radread <ellen.buddle@gmail.com>
2015-01-28 21:04:10 +0000

    set functions with jQuery to add items to list and take input from field

--- javascript-tutorial-3/script.js
+++ javascript-tutorial-3/script.js
@@ -1,1 +1,11 @@
 /* Exercise 1: Wish list */
+$(document).ready(function() {
+	function addToList(item) {
+		$("ol#items").append("<li>" + item + "</li>");
+	}
+
+	$(document).on('click', '#add-to-list', function() {
+		addToList($('#item').val());
+
+	})
+});
