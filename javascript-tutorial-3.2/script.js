093c50a9840340270fcd00583259b4f6c2f89465
radread <ellen.buddle@gmail.com>
2015-03-09 22:55:38 +0000

    created function to add items to wishlist

--- javascript-tutorial-3.2/script.js
+++ javascript-tutorial-3.2/script.js
@@ -1,1 +1,10 @@
 /* Exercise 1: Wish list */
+$(document).ready(function() {
+  // here go all the listeners
+  // e.g. on click, mouseover, change etc
+});
+
+//create a function to add things to the wishlist
+function addToList(item) {
+	$("ol#items").append("<li>" + items + "</li>");
+}
