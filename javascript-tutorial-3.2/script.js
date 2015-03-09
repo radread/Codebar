ce3da4ce42a1702bb4f0d23d7ae1a0836c3d33ec
radread <ellen.buddle@gmail.com>
2015-03-09 23:10:53 +0000

    created event listener so addToList executes on click

--- javascript-tutorial-3.2/script.js
+++ javascript-tutorial-3.2/script.js
@@ -1,10 +1,16 @@
 /* Exercise 1: Wish list */
-$(document).ready(function() {
-  // here go all the listeners
-  // e.g. on click, mouseover, change etc
-});
+
 
 //create a function to add things to the wishlist
 function addToList(item) {
 	$("ol#items").append("<li>" + item + "</li>");
 }
+
+$(document).ready(function() {
+  // here go all the listeners
+  // e.g. on click, mouseover, change etc
+  //create an event listener that adds the text to the list
+  $(document).on('click', '#add-to-list', function() {
+  	addToList($('#item').val());
+  });
+});
