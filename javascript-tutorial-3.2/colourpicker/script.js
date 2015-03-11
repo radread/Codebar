246563a4139450066256418a7c1e4b3358835c57
radread <ellen.buddle@gmail.com>
2015-03-11 20:53:33 +0000

    created function setPreviewColor and event listener to take input from field

--- javascript-tutorial-3.2/colourpicker/script.js
+++ javascript-tutorial-3.2/colourpicker/script.js
@@ -1,1 +1,11 @@
 /* Exercise 2: Color picker */
+
+function setPreviewColor(color) {
+	$('.preview').css('background-color', color);
+}
+
+$(document).ready(function() {
+	$(document).on('keyup keydown keypress', '#color', function() {
+		setPreviewColor($('#color').val());
+	});
+});
