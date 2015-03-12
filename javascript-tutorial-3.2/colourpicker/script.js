201f5151ea6820a065389eba10bae0dc58c53a7f
radread <ellen.buddle@gmail.com>
2015-03-12 22:13:35 +0000

    created function addBox
    add a color to favourites grid

--- javascript-tutorial-3.2/colourpicker/script.js
+++ javascript-tutorial-3.2/colourpicker/script.js
@@ -5,8 +5,15 @@ function setPreviewColor(color) {
 	$('.color-code').text($('.preview').css('background-color'));
 }
 
+function addBox(color) {
+	$('#colors').prepend("<div class='item' style='background-color: " + color + ";'></div>");
+}
+
 $(document).ready(function() {
 	$(document).on('keyup keydown keypress', '#color', function() {
 		setPreviewColor($('#color').val());
 	});
+	//$(document).on('click', '#add-to-favorite', function() {
+	//	$('#colors').append("<div class='item' style 'background-color: " + color + ";'></div>")
+	//});
 });
