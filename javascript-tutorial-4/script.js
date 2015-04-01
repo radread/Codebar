d858f37ac361f380b113864b166c989d547403c4
radread <ellen.buddle@gmail.com>
2015-04-01 20:55:56 +0100

    added response.send() to gitGitHubInfo() AND EVERYTHING WORKS

--- javascript-tutorial-4/script.js
+++ javascript-tutorial-4/script.js
@@ -2,28 +2,25 @@
 
 function getGithubInfo(username) {
 	var response = new XMLHttpRequest();
-	// console.log(username);
+	console.log(username);
+	response.onreadystatechange = function() {
+		if (response.readyState == 4 && response.status == 200) {
+			showUser(JSON.parse(response.responseText));
+			console.log(response);
+		}
+	}
 	response.open("GET", "https://api.github.com/users/" + username, true);
+	response.send();
 	// console.log(response);
 	// response.send();
 	// console.log(response);
-	response.onreadystatechange=function() {
-		if (response.readyState==4 && response.status==200) {
-			return response;
-			showUser(JSON.parse(response.responseText));
-		}
-	}
-
+	// response.onreadystatechange = function() {
+	// 	if (response.readyState == 4 && response.status == 200) {
+	// 		showUser(JSON.parse(response.responseText));
+	// 		console.log(response);
+	// 	}
+	// }
 }
-
-
-//IGNORE THIS FOR NOW
-	// 	function(data) {
-	// 	response = data;
-	// 	showUser(data);
-	// });
-	// return response
-// }
 
 function showUser(user) {
 	$("#profile h2").append(user.login + " is Github user #" + user.id);
@@ -33,9 +30,9 @@ function showUser(user) {
 $(document).ready(function() {
 	$(document).on('keypress', '#username', function(e) {
 		if (e.which === 13) {
-			var person = getGithubInfo($('#username').val());
-			console.log(person);
-			showUser(JSON.parse(person.responseText));
+			getGithubInfo($('#username').val());
+			// console.log(person);
+			// showUser(JSON.parse(person.responseText));
 		}
 	});
 });
